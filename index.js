8b250eb289421ba38640ea58c9fea7c7d165c9e5
nrcool <nomanalam7214@gmail.com>
2022-04-27 09:23:25 +0200

    solution

---
+++ index.js
@@ -0,0 +1,18 @@
+// **Instructions**
+// 1. Declare a variable named `myArray` and store an array that contains 5 elements. Print the array.
+
+const myArray = ["Berlin", "Germany", true, 34, "Web dev"]; 
+
+console.table(myArray)
+
+
+// 2. Access the 3rd element in the array and save that to another variable named `thirdElement`. Print the `thirdElement`. 
+
+const thirdElement = myArray[2];
+console.log(thirdElement)
+
+// 3. Reassign the fourth element of the array and give it a value of "fourth element". Print the array once more. 
+
+myArray[3] = "fourthelement";
+
+console.log(myArray)
